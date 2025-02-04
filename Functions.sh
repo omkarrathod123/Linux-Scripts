ffae7a7d37c19404db6af2034b45eb0eeddcd810
omkarrathod123 <omkarrathod0705@gmail.com>
2025-02-04 17:12:36 +0530

    Functions

--- Functions.sh
+++ Functions.sh
@@ -5,3 +5,11 @@ statement(){
     echo "This Echo is proof of this Function"
 }
 statement
+
+Func1(){
+    echo "this Statement is in Function 1"
+}
+Func2(){
+    echo "This statement is in Function 2"
+    Func
+}
