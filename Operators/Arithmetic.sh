69ad2758f82f8e85191c94e09357f9878ca136e2
omkarrathod123 <omkarrathod0705@gmail.com>
2025-02-05 13:03:10 +0530

    Relational Operators in shell script

--- Operators/Arithmetic.sh
+++ Operators/Arithmetic.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 #All Arithmethic Operations in shell sctipts
+
+#------------- Take two variables-------------
+a=$1
+b=$2
+c=`expr $a + $b`
+echo "Addition is $c"
+
+c=`expr $a - $b`
+echo "Subtraction is $c"
+
+c=`expr $a \* $b`
+echo "Multiplacation is $c"
+
+c=`expr $a / $b`
+echo "Division is $c"
+
+c=`expr $a % $b`
+echo "Pointer is $c"
