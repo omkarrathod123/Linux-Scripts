8b159c1ad375958b6abce36244018b5041e12673
omkarrathod123 <omkarrathod0705@gmail.com>
2026-04-21 17:37:53 +0530

    Adding file commands to the repo

---
+++ file.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Demostration of basic file commands in linux
+
+touch test.txt  # touch commands used to create file.
+file test.txt #file commands determines type of file.
+pwd
+mkdir Test
+cp test.txt /Test/
+cd Test
+pwd
+echo"$?"
